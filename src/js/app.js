83c6d5d76f464085c90264a738b89294632470bd
Bhavya P <bhavyapchandran@gmail.com>
2022-05-14 13:10:38 -0400

    modified

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   var hot = document.querySelectorAll(".hot")
+  
   hot.forEach(item => {
     item.innerHTML += "🔥"
   })
